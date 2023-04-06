0e0dd738f9ba69e4c009ecf6d62373048e8d6237
look4abhinav <talk2abhinav@yahoo.com>
2023-04-06 22:29:39 +0530

    Added Testcases

--- test_chatbot.py
+++ test_chatbot.py
@@ -3,8 +3,28 @@ from chatbot import get_response
 
 
 class test_chatbot(unittest.TestCase):
-    def test_response(self):
-        sentences = ["Hi", "Hello", "How are you?", "Search hello"]
+    def test_greeting(self):
+        sentences = ["Hi", "Hello", "How are you?"]
+        for sentence in sentences:
+            assert get_response(sentence)
+
+    def test_goodbye(self):
+        sentences = ["Bye", "See you later", "Goodbye"]
+        for sentence in sentences:
+            assert get_response(sentence)
+
+    def test_thanks(self):
+        sentences = ["Thanks", "Thank you", "That's helpful"]
+        for sentence in sentences:
+            assert get_response(sentence)
+
+    def test_thanks(self):
+        sentences = ["Thanks", "Thank you", "That's helpful"]
+        for sentence in sentences:
+            assert get_response(sentence)
+
+    def test_thanks(self):
+        sentences = ["Tell me a joke", "joke", "Make me laugh"]
         for sentence in sentences:
             assert get_response(sentence)
 
